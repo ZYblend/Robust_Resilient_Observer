62f1e69dc8290f2cb75d9311b60e9c3da2a1b341
ZYblend <36635562+ZYblend@users.noreply.github.com>
2022-10-01 16:32:43 -0400

    update simulation result

--- plot_results.m
+++ plot_results.m
@@ -7,6 +7,7 @@
 time_vec  = out.logsout.getElement('x').Values.Time;
 
 % error vectors
+e = out.logsout.getElement('e_no_prior').Values.Data;
 e_exact   = out.logsout.getElement('e_exact_prior').Values.Data; 
 e_inexact   = out.logsout.getElement('e_AADL').Values.Data; 
 e_pruning   = out.logsout.getElement('e_pruning').Values.Data; 
@@ -18,6 +19,7 @@ PPV_eta = out.logsout.getElement('PPV_eta').Values.Data;
 
 
 %% Ploting/Visualization/Metric tables
+e = vecnorm(e,2,2);
 e_exact = vecnorm(e_exact,2,2);
 e_inexact = vecnorm(e_inexact,2,2);
 e_pruning = vecnorm(e_pruning,2,2);
@@ -28,19 +30,24 @@ figure(1) %
 LW = 1.3;  % linewidth
 FS = 12;   % font size
 
-subplot(3,1,1)
+subplot(4,1,1)
+plot(time_vec,e,'k.','LineWidth',LW);
+ylabel('e','FontSize',FS)
+title('LSE without Support Prior','FontSize',FS);
+
+subplot(4,1,4)
 plot(time_vec,e_exact,'k.','LineWidth',LW);
 ylabel('e','FontSize',FS)
 ylim([0 0.2])
 title('LSE with Exact Support Prior','FontSize',FS);
 
-subplot(3,1,2)
+subplot(4,1,2)
 plot(time_vec,e_inexact,'k.','LineWidth',LW);
 ylabel('e','FontSize',FS)
 ylim([0 0.2])
 title('LSE with AADL Support Prior','FontSize',FS);
 
-subplot(3,1,3)
+subplot(4,1,3)
 plot(time_vec,e_pruning,'k.','LineWidth',LW);
 ylabel('e','FontSize',FS)
 xlabel('time','FontSize',FS)
